0307f24d4a1711bb2b9a12ad039a97ecd085668d
Codekacode <karem.quiroz.m@gmail.com>
2022-01-19 17:42:56 -0500

    start todo list in basic components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { TodoCounter } from './components/TodoCounter';
+import TodoItem from './components/TodoItem';
+import TodoSearch from './components/TodoSearch';
+import TodoList from './components/TodoList';
+import CreateTodoButton from './components/CreateTodoButton';
+
+const todos = [ 
+  {id: 1, text: "Cortar cebolla", completed: false},
+  {id: 2, text: "Cortar tomate", completed: false},
+  {id: 3, text: "Cortar papa", completed: false},
+]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoCounter/>
+      <TodoSearch/>
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key={todo.id} text={todo.text}/>
+        ))}
+      </TodoList>
+      <CreateTodoButton/> 
+      
+    </>
   );
 }
 
---
+++ src/components/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem(props) {
+  return(
+    <li>
+      <span>c</span>
+      <p>{props.text}</p>
+      <span>x</span>
+    </li>
+  )
+}
+
+export default TodoItem
